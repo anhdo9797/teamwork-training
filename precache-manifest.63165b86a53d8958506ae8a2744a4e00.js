2ba0c68c26f7f3bee981e3a597f43ba724533dda
anhdo9797 <docongphuocanh.2697@gmail.com>
2020-08-25 15:08:00 +0700

    Updates

---
+++ precache-manifest.63165b86a53d8958506ae8a2744a4e00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f319359f9f12e8afa33e5411a4e962",
+    "url": "/http//anhdo9797.github.io/teamwork-teamwork-training/index.html"
+  },
+  {
+    "revision": "56694bf25a212e5c715f",
+    "url": "/http//anhdo9797.github.io/teamwork-teamwork-training/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9b9536cf58cbb507481d",
+    "url": "/http//anhdo9797.github.io/teamwork-teamwork-training/static/js/2.8b479baa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//anhdo9797.github.io/teamwork-teamwork-training/static/js/2.8b479baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56694bf25a212e5c715f",
+    "url": "/http//anhdo9797.github.io/teamwork-teamwork-training/static/js/main.f176dc64.chunk.js"
+  },
+  {
+    "revision": "b2f203e0344ead0f0dd4",
+    "url": "/http//anhdo9797.github.io/teamwork-teamwork-training/static/js/runtime-main.292d11e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/http//anhdo9797.github.io/teamwork-teamwork-training/static/media/logo.5d5d9eef.svg"
+  }
+]);
